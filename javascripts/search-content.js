a48776ba06ca852dd1b460d05394b2e7cc251db7
Karthik Raja <k.raja@accenture.com>
2012-11-16 09:28:49 +0530

    commit

--- javascripts/search-content.js
+++ javascripts/search-content.js
@@ -9,11 +9,42 @@ var feedbackText = "";
 function init() {
       $("#search").click(search);
 	$("#notHelpful").click(postQuestion);
-	initPagination();
+	
       gadgets.window.adjustHeight();
 }
 
+/**
+ * Callback function that displays the content.
+ *
+ * Gets called every time the user clicks on a pagination link.
+ *
+ * @param {int} page_index New Page index
+ * @param {jQuery} jq the container with the pagination links as a jQuery object
+ */
+function pageselectCallback(page_index, jq){
+	var new_content = jQuery('#hiddenresult div.firstdiv:eq('+page_index+')').clone();
+	$('#Searchresult').empty().append(new_content);
+	return false;
+}
 
+/** 
+ * Initialisation function for pagination
+ */
+function initPagination() {
+	// count entries inside the hidden content
+	var num_entries = jQuery('#hiddenresult div.firstdiv').length;
+	console.log("num_entries::"+num_entries);
+	// Create content inside pagination element
+	$("#Pagination").pagination(num_entries, {
+		callback: pageselectCallback,
+		items_per_page:3 // Show only one item per page
+	});
+ }
+
+// When document is ready, initialize pagination
+$(document).ready(function(){      
+	initPagination();
+});
 function postQuestion () {
 console.log("Hello i'm in helpful section");
 	$('#search-info').hide();
@@ -410,47 +441,6 @@ var totalPage=total_page_all;
  
 	
 }
-
-
-// This is a very simple demo that shows how a range of elements can
-// be paginated.
-// The elements that will be displayed are in a hidden DIV and are
-// cloned for display. The elements are static, there are no Ajax 
-// calls involved.
-
-/**
- * Callback function that displays the content.
- *
- * Gets called every time the user clicks on a pagination link.
- *
- * @param {int} page_index New Page index
- * @param {jQuery} jq the container with the pagination links as a jQuery object
- */
-function pageselectCallback(page_index, jq){
-	var new_content = jQuery('#hiddenresult div.firstdiv:eq('+page_index+')').clone();
-	$('#Searchresult').empty().append(new_content);
-	return false;
-}
-
-/** 
- * Initialisation function for pagination
- */
-function initPagination() {
-	// count entries inside the hidden content
-	var num_entries = jQuery('#hiddenresult div.firstdiv').length;
-	console.log("num_entries::"+num_entries);
-	// Create content inside pagination element
-	$("#Pagination").pagination(num_entries, {
-		callback: pageselectCallback,
-		items_per_page:3 // Show only one item per page
-	});
- }
-
-// When document is ready, initialize pagination
-$(document).ready(function(){      
-	initPagination();
-});
-
 
 // Perform a search and display the results
 function search() {
